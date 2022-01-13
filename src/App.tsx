dd7c41e98b502ca46500b1e37a7d7016b4cb2532
Chujun Lu <chujunlu@hotmail.com>
2022-01-13 19:01:18 +0800

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import { Notes } from './components/Notes'
 import { NoteForm } from './components/NoteForm';
 import { NotesProvider } from './hooks/notesContext'
+import { NotFound } from './components/NotFound';
 
 export function App() {
   return (
@@ -12,9 +13,10 @@ export function App() {
       <NotesProvider>
         <Routes>
           <Route path='/' element={<Notes />} />
-          <Route path='/:page' element={<Notes />} />
-          <Route path='/notes/:id' element={<NoteForm />} />
+          <Route path='/:page(\\d+)' element={<Notes />} />
+          <Route path='/notes/:id(\\d+)' element={<NoteForm />} />
           <Route path='/create-note' element={<NoteForm />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </NotesProvider>
     </div>
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export function NotFound () {
+    return (
+      <div>
+        <h1>404! The page you are looking for was not found.</h1>
+        <p>
+          <Link to="/">Go home</Link>
+        </p>
+      </div>
+    );
+}
